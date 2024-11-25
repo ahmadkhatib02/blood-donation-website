acfdfab124bcf73df6830646c742921c8868b60d
ahmadkhatib02 <ahmad.alkhatib02@lau.edu>
2024-11-25 15:00:29 +0200

    almosted ended find blood page started the login page

--- FrontEnd/js/bloodbanks.js
+++ FrontEnd/js/bloodbanks.js
@@ -1,6 +1,10 @@
 const redCross= document.getElementById("selection1")
 const hospital= document.getElementById("selection2")
 const buttons = document.querySelector(".buttons")
+const contact = document.getElementsByClassName("Contact")
+const name = document.getElementsByClassName("name")
+const adress = document.getElementsByClassName("adress")
+let booking = new Object();
 
 redCross.addEventListener("click",function(){
     //styling
@@ -37,3 +41,16 @@ hospital.addEventListener("click",function(){
     //adding the content
     
 })
+
+for(let i=0; i<contact.length; i++){
+    contact[i].addEventListener("click",function(){
+        book(booking,name[i].innerText, adress[i].innerText)
+        window.location.href="homePage.html"
+    })
+}
+
+function book (booking, name, adress){
+    booking["name"]= name
+    booking["adress"]= adress
+    console.log(booking)
+}
