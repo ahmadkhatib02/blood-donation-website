11162726c4ad702fbb7122e4cae225acbd2a8d37
ahmadkhatib02 <ahmad.alkhatib02@lau.edu>
2024-12-07 12:23:44 +0200

    added "edit profile page"

---
+++ FrontEnd/js/profile-recipient-edit.js
@@ -0,0 +1,19 @@
+const submitEl = document.getElementById("submit-btn")
+const submit = document.getElementById("submit")
+const img = document.getElementById("img")
+const form = document.querySelector("form")
+
+
+submitEl.addEventListener("mouseover", function(){
+    submitEl.style.backgroundColor="black"
+    submitEl.style.cursor="pointer"
+    submit.style.color="white"
+    img.innerHTML=`<img src="images/check-circle-dark.png">`
+
+})
+
+submitEl.addEventListener("mouseout", function(){
+    submitEl.style.backgroundColor="white"
+    submit.style.color="black"
+    img.innerHTML=`<img src="images/check-circle.png">`
+})
